7ab11c0324d74646e58ab2282a5b611ba4b44e3a
sethcarrino <sethcarrino@gmail.com>
2017-09-29 13:42:28 -0400

    refactored code to use redux thunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,8 @@
+import firebase from 'firebase';
 import { 
 	EMAIL_CHANGED, 
-	PASSWORD_CHANGED 
+	PASSWORD_CHANGED,
+	LOGIN_USER_SUCCESS
 } from './types';
 
 export const emailChanged = (text) => {
@@ -16,3 +18,13 @@ export const passwordChanged = (text) => {
 		payload: text
 	};
 };
+
+export const loginUser = ({ email, password }) => {
+	return (dispatch) => {
+	firebase.auth().signInWithEmailAndPassword(email, password)
+		.then(user => {
+			dispatch({ type: LOGIN_USER_SUCCESS, payload: user });
+		});
+	};
+};
+
